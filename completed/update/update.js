6c920986ae4831d470ba3c2fb3db35e7a7cd1852
Bob D'Errico <rderrico@50onred.com>
2016-09-16 19:28:15 -0400

    Refactors iterateRecursively function to make a copy of the object, with a currently non-functional check for changes

--- completed/update/update.js
+++ completed/update/update.js
@@ -1,29 +1,34 @@
 function update(state, changes) {
   'use strict';
   
-  console.log(state);
   var nextState = {};
-
-  iterateRecursively(state, function(value) {
-    console.log(value);
-  });
+  
+  console.log('state', state);
+  nextState = iterateRecursively(state, changes);
+  console.log('nextState', nextState);
   
 }
 
-function iterateRecursively(object, callback) {
-  for (var property in object) {
-    // checking to make sure we are not iterating over properties further up 
-    // in the prototype chain
-    if (object.hasOwnProperty(property)) {
-      // there is a child object: call this function recursively
-      // otherwise, run the callback function
-      if (typeof object[property] === 'object') {
-        iterateRecursively(object[property], callback);
-      } else {
-        callback(object[property]);
-      } 
+function iterateRecursively(object, changes) {
+  var newState = {};
+  
+  // confirm that we have an object, if so, we need iterate over its properties
+  if (object instanceof Object) {
+    for (var property in object) {
+      // check to make sure we are not operating on prototype properties
+      if (object.hasOwnProperty(property)) {
+        console.log('changes:', changes[property]);
+        if (changes[property] instanceof Object && changes[property].hasOwnProperty('$set')) {
+          console.log('We have a change to make!', changes[property].$set);
+        }
+        // assign the property to the copy by calling the function recursively
+        newState[property] = iterateRecursively(object[property], changes[property]);
+      }
     }
-  }
+    return newState;
+  } 
+  // not an object, just return it.
+  return object;
 }
 
 module.exports = update;
