2bbdff4acd7c0a72a842ea965b101cdba7722322
Bob D'Errico <bob@bobderrico.com>
2016-09-17 12:43:26 -0400

    First test passing, callback function waits till threshold has passed before executing

--- completed/debounce/debounce.js
+++ completed/debounce/debounce.js
@@ -1,5 +1,12 @@
-function debounce(callback) {
+function debounce(callback, threshold) {
   'use strict';
+
+  // returns the debounced function
+  return function() {
+    // when called, wait for the threshold to pass, then execute the callback
+    setTimeout(callback, threshold);
+  };
+
 }
 
 module.exports = debounce;
