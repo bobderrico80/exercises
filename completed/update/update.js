b1d68061386b5b92f68d5e16072c0d9434278184
Bob D'Errico <bob@bobderrico.com>
2016-09-17 09:57:08 -0400

    Makes the  operation work. First test is passing

--- completed/update/update.js
+++ completed/update/update.js
@@ -1,32 +1,32 @@
 function update(state, changes) {
   'use strict';
-  
+
   var nextState = {};
-  
-  console.log('state', state);
+
   nextState = iterateRecursively(state, changes);
-  console.log('nextState', nextState);
-  
+  return nextState;
+
 }
 
 function iterateRecursively(object, changes) {
   var newState = {};
-  
+
   // confirm that we have an object, if so, we need iterate over its properties
   if (object instanceof Object) {
     for (var property in object) {
       // check to make sure we are not operating on prototype properties
       if (object.hasOwnProperty(property)) {
-        console.log('changes:', changes[property]);
         if (changes[property] instanceof Object && changes[property].hasOwnProperty('$set')) {
-          console.log('We have a change to make!', changes[property].$set);
+          // We have a change to make! Get the value of $set & assign to the new state.
+          newState[property] = changes[property].$set;
+          return newState;
         }
         // assign the property to the copy by calling the function recursively
         newState[property] = iterateRecursively(object[property], changes[property]);
       }
     }
     return newState;
-  } 
+  }
   // not an object, just return it.
   return object;
 }
