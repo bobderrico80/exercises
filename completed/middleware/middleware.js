b397846f80002cc72c5935b906e3ddc424fe9206
Bob D'Errico <bob@bobderrico.com>
2016-09-17 13:48:26 -0400

    Completes the middleware exercise.

--- completed/middleware/middleware.js
+++ completed/middleware/middleware.js
@@ -1,11 +1,32 @@
 var Middleware = function(){};
 
-Middleware.prototype.use = function(callback) {
+// array to store the middleware functions.
+var toProcess = [];
 
+// value to store the final callback
+var finalCallback;
+
+Middleware.prototype.use = function(middleware) {
+  // push middleware function into the stack
+  toProcess.push(middleware);
 };
 
 Middleware.prototype.go = function(callback) {
-
+  // save the final callback value
+  finalCallback = callback;
+  // process the middleware
+  processMiddleware();
 };
+
+function processMiddleware() {
+  // there is middleware to proces...
+  if (toProcess.length > 0) {
+    // shift middleware off the beginning of array, and call it, passing it this function.
+    toProcess.shift()(processMiddleware);
+  } else {
+    // if there is no more middleware, call the final callback.
+    finalCallback();
+  }
+}
 
 module.exports = Middleware;
