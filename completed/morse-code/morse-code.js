280ac688a7e1882b6ee3d6fe86ed53a352ae337b
Bob D'Errico <bob@bobderrico.com>
2016-09-17 14:08:15 -0400

    Sets some constants and variables. Converts message into a string of characters

--- completed/morse-code/morse-code.js
+++ completed/morse-code/morse-code.js
@@ -1,6 +1,22 @@
 function transmitter(options, callback) {
   'use strict';
 
+  // set some constants
+  var DOT = 1;
+  var DASH = DOT * 3;
+  var BUFFER = DOT;
+  var LETTER_SPACE = DOT * 3;
+  var WORD_SPACE = DOT * 7;
+
+  // set values from options
+  var toggle = options.toggle;
+  var codes = options.codes;
+  var timer = options.timeouter;
+
+  // convert message into an array of characters
+  var characters = options.message.split('');
+
 }
+
 
 module.exports = transmitter;
